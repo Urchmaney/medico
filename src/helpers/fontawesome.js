f9c6761c563280785365e5dccdf0169fd2007d19
Urchmaney <kingsobino@gmail.com>
2020-01-07 22:59:18 +0000

    Add fontawesome icons to library

---
+++ src/helpers/fontawesome.js
@@ -0,0 +1,5 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faChevronLeft, faCheckSquare, faSearch, faUser, faHeart, faSlidersH, faBell, faCheck } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab, faChevronLeft, faCheckSquare, faSearch, faUser, faHeart, faSlidersH, faBell, faCheck);
