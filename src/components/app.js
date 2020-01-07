1f5d4283fbd87cc05fac7797f6a9836f678e655e
Urchmaney <kingsobino@gmail.com>
2020-01-07 22:59:00 +0000

    Add routes for all pages

--- src/components/app.js
+++ src/components/app.js
@@ -6,12 +6,29 @@ import {
 } from 'react-router-dom';
 import Login from './login';
 import Home from './home';
+import Register from './register';
+import Dashboard from './dashboard';
+import DoctorsList from './doctor-list';
+import DoctorProfile from './doctor-profile';
+import '../helpers/fontawesome';
+import DatePicker from './date-picker';
+import Confirmation from './confirmation';
+import Appointments from './appointments';
+import Success from './success';
 
 const App = () => (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/doctors" component={DoctorsList} />
+      <Route path="/profile" component={DoctorProfile} />
+      <Route path="/date" component={DatePicker} />
+      <Route path="/confirm" component={Confirmation} />
+      <Route path="/appointments" component={Appointments} />
+      <Route path="/success" component={Success} />
     </Switch>
   </Router>
 );
