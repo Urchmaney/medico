cd24722b12a10038a243b94d2f7e385f1e865fa8
Urchmaney <kingsobino@gmail.com>
2020-01-13 23:03:07 +0000

    Make success a presentation component

---
+++ src/components/presentation/success.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Header from './header';
+import Status from './status';
+import imgSrc from '../../images/success.svg';
+import '../../styles/style.scss';
+
+const Success = (props) => {
+  const { docName, date, time } = props;
+  return (
+    <main>
+      <div className="container success">
+        <Header name="Success" />
+        <Status detail payment confirm />
+        <div className="img-con">
+          <img src={imgSrc} alt="success" />
+        </div>
+        <h3>
+          Appointment Confirmed
+        </h3>
+        <p>
+          Your appointment with Dr&nbsp;
+          {docName}
+          &nbsp;on&nbsp;
+          {date}
+          &nbsp;at&nbsp;
+          {time}
+          &nbsp;is confirmed.
+        </p>
+        <hr />
+        <Link to="/dashboard"> Home </Link>
+      </div>
+    </main>
+  );
+};
+
+Success.propTypes = {
+  docName: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired,
+};
+
+export default Success;
