f7bb00ef57e7d370c002411def6b6b816887e484
Urchmaney <kingsobino@gmail.com>
2020-01-13 23:08:42 +0000

    Integrate date and appointment page with api data.

--- src/components/appointments.js
+++ src/components/appointments.js
@@ -1,20 +1,66 @@
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Appointment from './presentation/appointment';
 import Header from './presentation/header';
+import { get } from '../helpers/api';
+import { appointmentsUrl } from '../helpers/constants';
 
-const app = [1, 3, 4, 5];
 
-const Appointments = () => (
-  <main>
-    <div className="container">
-      <Header name="Appointments" />
-      <div className="appoint-list-con">
-        {app.map(e => (
-          <Appointment key={e} date={new Date()} />
-        ))}
-      </div>
-    </div>
-  </main>
-);
+class Appointments extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      appointments: [],
+    };
+  }
 
-export default Appointments;
+  componentDidMount() {
+    const { token } = this.props;
+    const getResult = get(appointmentsUrl, token);
+    getResult.then(result => {
+      if (result.error) {
+        console.log('error');
+      } else {
+        this.setState(state => ({
+          ...state, appointments: result,
+        }));
+      }
+    });
+  }
+
+  render() {
+    const { appointments } = this.state;
+    const { history, loggedIn } = this.props;
+    if (!loggedIn) {
+      return (<Redirect to="/login" />);
+    }
+    return (
+      <main>
+        <div className="container">
+          <Header name="Appointments" iconOnClick={() => history.goBack()} />
+          <div className="appoint-list-con">
+            {appointments.map(e => (
+              <Appointment key={e.id} date={e.date} name={`${e.doctor.first_name} ${e.doctor.last_name}`} />
+            ))}
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  token: state.token,
+  loggedIn: state.loggedIn,
+});
+
+Appointments.propTypes = {
+  token: PropTypes.string.isRequired,
+  history: PropTypes.object.isRequired,
+  loggedIn: PropTypes.bool.isRequired,
+};
+
+export default connect(mapStateToProps)(Appointments);
--- src/components/date-picker.js
+++ src/components/date-picker.js
@@ -1,54 +1,120 @@
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Header from './presentation/header';
+import {
+  changeAppointmentDate,
+  changeAppointmentTime,
+} from '../actions/index';
 import '../styles/style.scss';
 import Button from './presentation/button';
 
-const times = ['10:30AM', '11:30AM', '12:30PM', '1:30PM'];
+class DatePicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: '',
+      time: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-const DatePicker = () => (
-  <main>
-    <div className="date-time">
-      <Header name="Booking" />
-      <form>
-        <input type="date" />
-        <div className="time-con">
-          {times.map(time => (
-            <div key={time}>{time}</div>
-          ))}
-        </div>
-        <div className="btn-con">
-          <Button name="Book Appointment" />
-        </div>
-      </form>
-      <div className="info-con">
-        <div className="info">
-          <h4>
-            Dr.&nbsp;
-            John Doe
-          </h4>
-          <p className="role">
-            Managing Director
-          </p>
-          <p className="feature">
-            <span>
-              &#8358;
-              200
-            </span>
-            <span>
-              120
-              &nbsp;yrs&nbsp; of&nbsp; exp
-            </span>
-            <span>
-              <span className="heart"><FontAwesomeIcon icon="heart" /></span>
-              &nbsp;
-              333
-            </span>
-          </p>
-        </div>
-      </div>
-    </div>
-  </main>
-);
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState(state => ({
+      ...state, [name]: value,
+    }));
+  }
 
-export default DatePicker;
+  handleSubmit() {
+    const { date, time } = this.state;
+    const {
+      changeAppointmentDate,
+      changeAppointmentTime,
+      history,
+    } = this.props;
+    changeAppointmentDate(date);
+    changeAppointmentTime(time);
+    history.push('/confirm');
+  }
+
+  render() {
+    const { doctor, role, history, loggedIn } = this.props;
+    const { handleChange, handleSubmit } = this;
+    if (!loggedIn) {
+      return (<Redirect to="/login" />);
+    }
+    return (
+      <main>
+        <div className="date-time">
+          <Header name="Booking" iconOnClick={() => history.goBack()} />
+          <form>
+            <label htmlFor="date">
+               Date
+              <input id="date" type="date" onChange={handleChange} name="date" />
+            </label>
+            <label htmlFor="time">
+            Time
+              <input id="time" type="time" name="time" onChange={handleChange} />
+            </label>
+            <div className="btn-con">
+              <Button name="Book Appointment" onClick={handleSubmit} />
+            </div>
+          </form>
+          <div className="info-con">
+            <div className="info">
+              <h4>
+                Dr.&nbsp;
+                {`${doctor.first_name} ${doctor.last_name}`}
+              </h4>
+              <p className="role">
+                {role.name}
+              </p>
+              <p className="feature">
+                <span>
+                  &#8358;
+                  {doctor.price}
+                </span>
+                <span>
+                  {doctor.years_experience}
+                  &nbsp;yrs&nbsp; of&nbsp; exp
+                </span>
+                <span>
+                  <span className="heart"><FontAwesomeIcon icon="heart" /></span>
+                  &nbsp;
+                  {doctor.likes_count}
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  changeAppointmentDate: (date) => dispatch(changeAppointmentDate(date)),
+  changeAppointmentTime: (time) => dispatch(changeAppointmentTime(time)),
+});
+
+const mapStateToProps = state => ({
+  doctor: state.doctor,
+  role: state.role,
+  loggedIn: state.loggedIn,
+});
+
+DatePicker.propTypes = {
+  doctor: PropTypes.object.isRequired,
+  role: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired,
+  changeAppointmentDate: PropTypes.func.isRequired,
+  changeAppointmentTime: PropTypes.func.isRequired,
+  loggedIn: PropTypes.bool.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DatePicker);
